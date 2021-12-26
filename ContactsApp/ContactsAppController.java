9fd876abc36166b83593416e49da5a0f5b5d06f8
kaikamal <95711390+kaikamal@users.noreply.github.com>
2021-12-26 19:13:22 +0600

    Add files via upload

---
+++ ContactsApp/ContactsAppController.java
@@ -0,0 +1,116 @@
+package com.example.demo2;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ContactsAppController {
+    private ObservableList<Contact> contacts = FXCollections.observableArrayList();
+    private Contact contactSelected;
+
+    @FXML
+    private ListView<Contact> ContactsListView;
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField phoneNumberTextField;
+
+    public void initialize(){
+        contacts.add(new Contact("Yoshida","Nao","nao.yoshida@gmail.com","+77025852719"));
+        contacts.add(new Contact("Hirose","Madoka","madoka.hirose@gmail.com","+7709823477"));
+        contacts.add(new Contact("Tscuchiya","Ryota","ryotaaa@gmail.com","+77874578723"));
+        contacts.add(new Contact("Hino","Tetsuo","hinooo@gmail.com","+7798578237"));
+        ContactsListView.setItems(contacts);
+        ContactsListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Contact>() {
+            @Override
+            public void changed(ObservableValue<? extends Contact> arg0, Contact oldValue, Contact newValue) {
+                contactSelected = newValue;
+                firstNameTextField.setText(newValue.getFirstName());
+                lastNameTextField.setText(newValue.getLastName());
+                emailTextField.setText(newValue.getEmail());
+                phoneNumberTextField.setText(newValue.getPhoneNumber());
+            }
+        });
+    }
+
+    public boolean validationFailed() {
+        return (firstNameTextField.getText().trim().equals("") ||
+                lastNameTextField.getText().trim().equals("") ||
+                emailTextField.getText().trim().equals("") ||
+                phoneNumberTextField.getText().trim().equals(""));
+    }
+    @FXML
+    void addButtonPressed(ActionEvent event) {
+        try{
+            if (validationFailed()){
+                throw new NumberFormatException();
+            }
+            else{
+                Contact obj1 = new Contact();
+                obj1.setFirstName(firstNameTextField.getText());
+                obj1.setLastName(lastNameTextField.getText());
+                obj1.setEmail(emailTextField.getText());
+                obj1.setPhoneNumber(phoneNumberTextField.getText());
+                contacts.add(obj1);
+            }
+        } catch (Exception ex){
+            firstNameTextField.setText("");
+            lastNameTextField.setText("");
+            emailTextField.setText("");
+            phoneNumberTextField.setText("");
+            firstNameTextField.requestFocus();
+        }
+
+    }
+
+    @FXML
+    void deleteButtonPressed(ActionEvent event) {
+        try {
+            contacts.remove(contactSelected);
+        }
+        catch (Exception ex){
+            firstNameTextField.setText("");
+            lastNameTextField.setText("");
+            emailTextField.setText("");
+            phoneNumberTextField.setText("");
+            firstNameTextField.requestFocus();
+        }
+    }
+
+    @FXML
+    void updateButtonPressed(ActionEvent event) {
+        try{
+            if (validationFailed()){
+                throw new NumberFormatException();
+            }
+            else{
+                Contact obj1 = new Contact();
+                contactSelected.setFirstName(firstNameTextField.getText());
+                contactSelected.setLastName(lastNameTextField.getText());
+                contactSelected.setEmail(emailTextField.getText());
+                contactSelected.setPhoneNumber(phoneNumberTextField.getText());
+
+            }
+        } catch (Exception ex){
+            firstNameTextField.setText("");
+            lastNameTextField.setText("");
+            emailTextField.setText("");
+            phoneNumberTextField.setText("");
+            firstNameTextField.requestFocus();
+        }
+    }
+
+}
